1453a22f8d11908ca04d808d79a68eda972abf1b
k <you@example.com>
2019-01-14 18:37:35 +0100

    finish?

--- publish.py
+++ publish.py
@@ -1,11 +1,10 @@
 #!env python3
-import ipfsapi
+from executor import execute
 import lazydb
 import shutil
 import subprocess
 import tempfile
 import json
-import youtube_dl
 import sys
 import optparse
 import blogger
@@ -17,36 +16,33 @@ import jinja2
 import re
 
 
-VIDEOS_DIR = "/mnt/sadhguru"
-TEMPLATE = '''
-<br />
-
-<div style="padding-bottom: 56.25%; position: relative;">
-  <iframe allow="encrypted-media" allowfullscreen="" frameborder="0" gesture="media" src="https://www.youtube.com/embed/{{ video_id }}" style="height: 100%; left: 0; position: absolute; top: 0; width: 100%;">
-  </iframe>
-</div>
-
-<ul>
-{% for file in files %}
-<li>Download from IPFS: <a href="https://cloudfare-ipfs.com/ipfs/{{ ipfs_hash }}">{{ title }}</a></li>
-{% endfor %}
-</ul>
-</div>
-'''
-
-
 class Video:
-    def __init__(self, youtube_id, title, description, uploader):
+    def __init__(self, youtube_id, title, description, uploader, file):
         self.youtube_id = youtube_id
         self.description = description
         self.title = title
         self.uploader = uploader
-        self.files = []
+        self.file = file
+    def __repr__(self):
+        return self.youtube_id
 
 def publish_random_video(db):
 
-    def publish_video(video, labels="sadhguru, video"):
-        template = jinja2.Template(TEMPLATE)
+    def publish_video(video):
+        template_raw = '''
+        <br />
+        <div style="padding-bottom: 56.25%; position: relative;">
+        <iframe allow="encrypted-media" allowfullscreen="" frameborder="0" gesture="media" src="https://www.youtube.com/embed/{{ video_id }}" style="height: 100%; left: 0; position: absolute; top: 0; width: 100%;">
+        </iframe>
+        </div>
+        <ul>
+        {% for file in files %}
+            <li>Download from IPFS: <a href="https://cloudfare-ipfs.com/ipfs/{{ ipfs_hash }}">{{ title }}</a></li>
+        {% endfor %}
+        </ul>
+        </div>
+        '''
+        template = jinja2.Template(template_raw)
         html = template.render(
             video_id=video.youtube_id,
             title=video.title,
@@ -56,8 +52,9 @@ def publish_random_video(db):
             clientId="62814020656-olqaifiob7ufoqpe1k4iah3v2ra12h8a.apps.googleusercontent.com", 
             clientSecret = "fnUgEpdkUTtthUtDk0vLvjMm",
             blogId = "8804984470189945822")
-
+        labels = "video, " + video.uploader
         eb.post(video.title, html, labels, isDraft=False)
+
     current_videos = list(db.keys())
     if not current_videos:
         return
@@ -68,83 +65,34 @@ def publish_random_video(db):
 
       
 def check_for_videos(db, url):
-    class MyLogger:
-        def __init__(self):
-            self.output = []
-        def debug(self, msg): 
-            print(msg) 
-            #hack: filter out real debug messages, leave only "info" messages
-            if re.match("^\[.*\]", msg):
-                return
-            self.output.append(msg)
 
     def find_new_videos_ids(url):
-        logger = MyLogger()
-        opts = dict(
-            forceid=True,
-            simulate=True,
-            quiet=True,
-            logger=logger
-        )
-        with youtube_dl.YoutubeDL(opts) as ydl:
-            ydl.download(url)
-
+        result = execute("youtube-dl --get-id " + url, capture=True)
         local_videos_ids = set(db.keys())
-        remote_videos_ids = set(logger.output)
+        remote_videos_ids = set(result.splitlines())
         new_videos_ids = remote_videos_ids - local_videos_ids
         return new_videos_ids
 
-    def download_video(video_id, skip_download=False):
-        logger = MyLogger()
-        ytd_opts = dict(
-           logger=logger,
-           forceid=True,
-           skipdownload=skip_download,
-           writeinfojson=True,
-           ignoreerrors=True,
-           playlistrandom=True,
-#          format='(248,247/bestvideo[ext=webm])+bestaudio',
-           outtmpl='%(uploader)s - %(title)s (%(height)sp) [%(id)s].%(ext)s'
-        )
+    def download_video(video_id):
         with tempfile.TemporaryDirectory() as tmpdir:
-            original_cwd = os.getcwd()
             os.chdir(tmpdir)
-            with youtube_dl.YoutubeDL(ytd_opts) as ytd:
-                ytd.download([video_id])
-
+            execute("youtube-dl --write-info-json --ignore-errors --playlist-random"\
+                " --output '%(uploader)s - %(title)s (%(height)sp) [%(id)s].%(ext)s' " + video_id)
+            
             video_json = json.load(open(glob.glob("*.json")[0]))
             video = Video(
                 video_id,
                 video_json["title"],
                 video_json["description"],
-                video_json["uploader"]
+                video_json["uploader"],
+                file = [file for file in glob.glob("*") if not file.endswith(".json")][0]
             )
-            video.files = [file for file in glob.glob("*") if not file.endswith(".json")]
             db.put(video.youtube_id, video)
-            for file in video.files:
-                shutil.move(file, original_cwd)
-
-#           TORRENT:
-#            for file in video.files:
-#                torrent_name = os.path.splitext(file)[0] + ".torrent"
-#                cmd = ["transmission-create", "-o", torrent_name]
-#
-#                for tracker in open(original_cwd + "/torrent-trackers.txt").readlines():
-#                    cmd += ["-t", tracker.strip()]
-#
-#                cmd += [file]
-#
-#                subprocess.check_call(cmd)
-#                cmd = ["transmission-remote", "--add", "'%s'" % torrent_name,
-#                        "--auth", "transmission:transmission"]
-#                subprocess.check_call(cmd)
-#                
-#                shutil.move(torrent_name, original_cwd)
 
         
-    new_videos_ids = find_new_videos_ids([url])
+    new_videos_ids = find_new_videos_ids(url)
     for video_id in new_videos_ids:
-        download_video(video_id, skip_download=True)
+        download_video(video_id)
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
