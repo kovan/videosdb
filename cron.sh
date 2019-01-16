7b4b1a5dc538fecb312db21ba1d4a078f1b76b30
Your Name <you@example.com>
2019-01-16 16:45:43 +0000

    fix cron

--- cron.sh
+++ cron.sh
@@ -3,5 +3,6 @@
 cd ~/prj/blog
 git pull
 source venv/bin/activate
-/videos_db.py --enqueue https://www.youtube.com/user/sadhguru
-/videos_db.py --publish-next
+pip install -r requirements.txt
+python videos_db.py --enqueue https://www.youtube.com/user/sadhguru
+python videos_db.py --publish-next
