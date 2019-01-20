4133472c4c37f4e26cf4181e7febe8e8f0595ca4
Your Name <you@example.com>
2019-01-20 10:25:28 +0000

    cron is now run

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 
-git pull
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -q -r requirements.txt
 python videos_db.py --verbose --enqueue https://www.youtube.com/user/sadhguru
 python videos_db.py --verbose --ipfs-enable --publish-next
