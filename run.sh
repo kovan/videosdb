74e04e3c6f9362656136b949a5d9b58e26064090
Your Name <you@example.com>
2019-01-21 15:29:14 +0000

    on production don't be verbose

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
 source venv/bin/activate
 pip install -q -r requirements.txt
-python videos_db.py --verbose --enqueue https://www.youtube.com/user/sadhguru
-python videos_db.py --verbose --ipfs-enable --publish-next
+python videos_db.py  --enqueue https://www.youtube.com/user/sadhguru
+python videos_db.py  --ipfs-enable --publish-next
