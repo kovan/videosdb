d852df699969afc8b1da98ffef484bdf069dc7ad
Your Name <you@example.com>
2019-01-23 13:46:26 +0000

    add freezed requirements

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 source venv/bin/activate
-pip install -q -r requirements.txt
+pip install -q -r requirements.freezed.txt
 export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
 python videos_db.py  --enqueue https://www.youtube.com/user/sadhguru
 python videos_db.py  --enable-ipfs --publish-next
