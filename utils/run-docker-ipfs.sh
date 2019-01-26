469c6a4fee4b04835e0d8faa6ca2dc1cd731e796
Your Name <you@example.com>
2019-01-26 11:09:14 +0000

    utils

--- utils/run-docker-ipfs.sh
+++ utils/run-docker-ipfs.sh
@@ -6,8 +6,10 @@ rm -fr $DIR2
 mkdir $DIR1
 mkdir $DIR2
 
-docker run -d --name ipfs-node \
+CID=$(docker run -d --name ipfs-node \
 	--restart always\
-  -v $DIR2:/export -v $DIR1:/data/ipfs \
-  -p 80:8080 -p 4001:4001 -p 5001:5001 \
-  jbenet/go-ipfs:latest
+        --network ee-global-frontend-network\
+   -v $DIR2:/export -v $DIR1:/data/ipfs \
+   -p 4001:4001 -p 5001:5001 \
+  jbenet/go-ipfs:latest)
+docker exec $CID ipfs config profile apply server
