6e22fcc8895f355bf56440212eeed0c7162d310d
Your Name <you@example.com>
2019-01-27 21:32:37 +0000

    changes

--- utils/run-docker-ipfs.sh
+++ utils/run-docker-ipfs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 DIR1=/mnt/ipfs-docker-data
 DIR2=/mnt/ipfs-docker-staging
-rm -fr $DIR1
-rm -fr $DIR2
-mkdir $DIR1
-mkdir $DIR2
+#rm -fr $DIR1
+#rm -fr $DIR2
+#mkdir $DIR1
+#mkdir $DIR2
 
 CID=$(docker run -d --name ipfs-node \
 	--restart always\
         --network ee-global-frontend-network\
+        --ip 172.19.0.100\
    -v $DIR2:/export -v $DIR1:/data/ipfs \
    -p 4001:4001 -p 5001:5001 \
   jbenet/go-ipfs:latest)
