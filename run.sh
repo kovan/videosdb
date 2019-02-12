775ca84e5bc35a5b624cd893e67ef40cbeaffe80
Your Name <you@example.com>
2019-02-12 17:31:27 +0000

    changes

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 source venv/bin/activate
 pip install -q -r requirements.freezed.txt
 #export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
-./manage.py videosdb -t -v --check-for-new-videos
-./manage.py videosdb -t -v --publish-next
+./manage.py videosdb -t --check-for-new-videos
+./manage.py videosdb -t --publish-next
