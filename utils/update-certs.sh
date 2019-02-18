0ea62edaa14014f19faa074dcf6c64379d4532e7
Your Name <you@example.com>
2019-02-18 10:07:33 +0000

    quiet certbot

--- utils/update-certs.sh
+++ utils/update-certs.sh
@@ -1,5 +1,5 @@
 DST=/var/lib/docker/volumes/global-nginx-proxy_dhparam/_data
-certbot renew
+certbot renew --quiet
 cp /etc/letsencrypt/live/ipfs.spiritualityresources.net/privkey.pem $DST/privkey.pem
 cp /etc/letsencrypt/live/ipfs.spiritualityresources.net/fullchain.pem $DST/fullchain.pem
 cp /etc/letsencrypt/live/resources.spiritualityresources.net/privkey.pem $DST/privkey-res.pem
