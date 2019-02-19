98e643fc3cb6b2559e027dba1a25701de38865a6
Your Name <you@example.com>
2019-02-19 22:01:33 +0000

    move to pipenv

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-
-source venv/bin/activate
-pip install -q -r requirements.freezed.txt
+#pipenv activate
+#source venv/bin/activate
+pipenv shell
+pipenv install
+#pip install -q -r requirements.freezed.txt
 #export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
 ./manage.py videosdb -t --check-for-new-videos
 ./manage.py videosdb -t --publish-next
