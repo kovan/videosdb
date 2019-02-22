2f4f10f0387c2aa7c756da6739d2f7e7537774db
Your Name <you@example.com>
2019-02-22 22:20:16 +0000

    run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 pipenv run pipenv install
-#export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
-pipenv run python manage.py videosdb -t --check-for-new-videos
-pipenv run python manage.py videosdb -t --publish-next
+export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
+pipenv run python manage.py videosdb --check-for-new-videos
+pipenv run python manage.py videosdb --publish-next
