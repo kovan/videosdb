5747c19e7804e388f3daacdfdc2cba062f927bee
Your Name <you@example.com>
2019-02-24 13:58:11 +0000

    creds

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-pipenv run pipenv install
 export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
+export GOOGLE_APPLICATION_CREDENTIALS=creds.json
+pipenv run pipenv install
 pipenv run python manage.py videosdb --check-for-new-videos
 pipenv run python manage.py videosdb --publish-next
