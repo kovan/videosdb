bc96d75ef4209c83d109e1080b6d9ff14986659b
Your Name <you@example.com>
2019-02-26 21:31:32 +0000

    fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
 export GOOGLE_APPLICATION_CREDENTIALS=creds.json
 source venv/bin/activate
+pip install --upgrade pip
 pip install -q -r requirements.freezed.txt
 ./manage.py videosdb -t --check-for-new-videos
 ./manage.py videosdb -t --publish-next
