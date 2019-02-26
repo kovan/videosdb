14e89ba99f985aa60e6b5c42c328a5d871f09a2a
Your Name <you@example.com>
2019-02-26 09:23:19 +0000

    Revert "move to pipenv"
    This reverts commit 98e643fc3cb6b2559e027dba1a25701de38865a6.

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
-#pipenv activate
-#source venv/bin/activate
-pipenv shell
-pipenv install
-#pip install -q -r requirements.freezed.txt
+
+source venv/bin/activate
+pip install -q -r requirements.freezed.txt
 #export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
 ./manage.py videosdb -t --check-for-new-videos
 ./manage.py videosdb -t --publish-next
