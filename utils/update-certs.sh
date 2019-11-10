6c2b2e48f113e20a53a2c1ca2d1358097711bae8
k <pi@raspberrypi>
2019-11-10 08:52:23 +0100

    changes

--- utils/update-certs.sh
+++
@@ -1,7 +0,0 @@
-DOMAIN=enlightenment.yoga
-SRC=/etc/letsencrypt/live/$DOMAIN
-DST=/var/lib/docker/volumes/global-nginx-proxy_certs/_data
-
-certbot renew --quiet
-cp $SRC/fullchain.pem $DST/$DOMAIN.fullchain.pem
-cp $SRC/privkey.pem $DST/$DOMAIN.privkey.pem
