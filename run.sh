533ed60d8fd9dd6508d664ab0ace1df09a8198b1
k <pi@raspberrypi>
2019-11-11 17:53:22 +0100

    changes

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 export AUTOLOGGING_TRACED_NOOP=1 # disable autologging
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/creds.json
 source venv/bin/activate
-pip install -q --upgrade pip
-pip install -q -r requirements.freezed.txt
 ./manage.py videosdb -t --check-for-new-videos
 ./manage.py videosdb -t --publish-next
