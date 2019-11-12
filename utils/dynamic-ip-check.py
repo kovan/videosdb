41f44265150eb5470fb8dd6f72b478deeb0a4f9c
k <pi@raspberrypi>
2019-11-12 10:47:19 +0100

    fix-bug

--- utils/dynamic-ip-check.py
+++ utils/dynamic-ip-check.py
@@ -7,12 +7,12 @@ from executor import execute
 
 config = yaml.safe_load(open("config.yaml"))
 hostname = config["dns_root"]
-published_ip = socket.gethostbyname(hostname) + "3"
+published_ip = socket.gethostbyname(hostname)
 my_public_ip = requests.get('https://api.ipify.org').text
 
 if published_ip != my_public_ip:
     execute("python manage.py videosdb --update-ip " + my_public_ip)
-    print("Updated IP to " + my_public_ip)
+    print("Updated IP from + " + published_ip + " to " + my_public_ip)
     time.sleep(300)
 
 
