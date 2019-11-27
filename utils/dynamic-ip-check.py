3fc8387f5abf3b2f57839b3c507b77888b602ecf
Your Name <you@example.com>
2019-11-27 02:51:49 +0100

    changes

--- utils/dynamic-ip-check.py
+++ utils/dynamic-ip-check.py
@@ -9,7 +9,6 @@ config = yaml.safe_load(open("config.yaml"))
 hostname = config["dns_root"]
 published_ip = socket.gethostbyname(hostname)
 my_public_ip = requests.get('https://api.ipify.org').text
-
 if published_ip != my_public_ip:
     execute("python manage.py videosdb --update-ip " + my_public_ip)
     print("Updated IP from + " + published_ip + " to " + my_public_ip)
