f5a243e2f1c4f57e45a9b8d5571abc314878bbe9
Tu Nombre <you@example.com>
2020-12-27 14:45:33 +0100

    desc

--- videosdb_code.py
+++ videosdb_code.py
@@ -124,6 +124,7 @@ https://www.youtube.com/watch?v={{youtube_id}}
         )
 
         post = WordPressPost()
+        post.excerpt = description
         post.title = video.title
         post.content = html
         post.custom_fields = [{
