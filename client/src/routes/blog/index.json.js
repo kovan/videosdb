7b999e6eaa6fc826a84a70f622537da5aac20cd4
Your Name <you@example.com>
2021-01-04 04:12:06 +0100

    changes

--- client/src/routes/blog/index.json.js
+++ client/src/routes/blog/index.json.js
@@ -1,11 +1,24 @@
 import posts from './_posts.js';
+import axios from "axios";
+// fetch("http://localhost:8000/api/publications")
+// .then(response => response.json())
+// .then(data => console.log(data));
 
-const contents = JSON.stringify(posts.map(post => {
-	return {
-		title: post.title,
-		slug: post.slug
-	};
-}));
+let contents = "";
+axios.get("http://localhost:8000/api/publications")
+  .then(function (response) {
+	  contents = JSON.stringify(response.data.results);
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 
 export function get(req, res) {
 	res.writeHead(200, {
