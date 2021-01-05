8b122ca0b08c0fe80cd81b42f4d1fa69b7995db3
Your Name <you@example.com>
2021-01-05 05:28:49 +0100

    categories showing

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -49,55 +49,10 @@
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 
-let linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 import axios from 'axios'
 
 async function getCategories() {
-  let categories = {}
+  let categories = []
   const response = await axios.get("http://localhost:8000/api/categories/")
   categories = response.data.results.map((category) => {
     return {
@@ -113,14 +68,23 @@ async function getCategories() {
 }
 
 
+
 export default {
   name: 'MainLayout',
   components: { EssentialLink },
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: getCategories()
+      essentialLinks: []
     }
+  },
+  mounted() {
+    return getCategories().then((cats) => {
+      cats.forEach( cat => {
+        this.essentialLinks.push(cat)
+      })
+
+    });
   }
 }
 </script>
