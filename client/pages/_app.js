3de5f6725b3ea17d22a10eb76e4aa70e87792682
Your Name <you@example.com>
2021-01-08 00:44:56 +0100

    working 1

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,24 +1,42 @@
 import '../styles/globals.css'
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from '../src/components/GlobalStyles';
 import '../src/mixins/chartjs';
 import theme from '../src/theme';
-import routes from '../src/routes';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  //return <Component {...pageProps} />
 
-  const routing = useRoutes(routes);
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  // React.useEffect(() => {
+  //   // Remove the server-side injected CSS.
+  //   const jssStyles = document.querySelector('#jss-server-side');
+  //   if (jssStyles) {
+  //     jssStyles.parentElement.removeChild(jssStyles);
+  //   }
+  // }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {routing}
-    </ThemeProvider>
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+
