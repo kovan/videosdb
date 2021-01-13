8738d07bc1c1a3383a35551443e61ea5413cdbb6
Your Name <you@example.com>
2021-01-13 11:43:06 +0100

    video working

--- pages/_video.vue
+++ pages/_video.vue
@@ -1,8 +1,26 @@
 <template lang="pug">
-    
+v-container
+    iframe(
+    width='560' 
+    height='315' 
+    :src="'https:/www.youtube.com/embed/' +this.video.youtube_id"
+    frameborder='0' 
+    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' 
+    allowfullscreen)
+
 </template>
 <script>
+import axios from "axios";
+
 export default {
-    
+ 
+  async asyncData({params}) { 
+    let video = await axios.get(
+       'http://localhost:8000/api/videos/' + params.video
+     )
+
+    video = video.data
+    return {video}
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,16 @@
 
   v-container
     v-container.grey.lighten-5.mb-6(align='center', fluid=true)
-      v-row(align='center' no-gutters='' style='height: 150px;')
+      v-row(align='center' no-gutters style='height: 150px;')
         v-col(v-for='video in this.videos' :key='youtube_id')
-          v-card.pa-2(outlined='' tile='')
+          v-card.pa-2(outlined tile)
             //- iframe(
             //-   width='560' 
             //-   height='315' 
             //-   :src="'https:/www.youtube.com/embed/' +video.youtube_id"
             //-   frameborder='0' 
             //-   allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' 
-            //-   allowfullscreen='')
+            //-   allowfullscreen)
 
             | {{video.title}}
 
@@ -21,7 +21,7 @@
 
           v-card-actions
             v-spacer
-              v-btn(color='primary' nuxt='' to='/inspire')
+              v-btn(color='primary' nuxt :to='video.slug')
                 | Continue
 </template>
 
