61ce934106a72ac4b5b6e49eb433a3dba976b6e9
Your Name <you@example.com>
2021-01-14 14:27:06 +0100

    revert to sqlite

--- project/settings.py
+++ project/settings.py
@@ -79,16 +79,26 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'videosdb',
+#         'USER': 'myuser',
+#         'PASSWORD': 'mypass',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#         'TEST': {
+#             "NAME": "test_videosdb",
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'videosdb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypass',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'TEST': {
-            "NAME": "test_videosdb",
+            "NAME": "dbtest.sqlite3",
         },
     }
 }
