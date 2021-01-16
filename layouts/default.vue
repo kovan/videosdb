cc9de10dc242ade27ec4a73daf95c88149e7f273
Your Name <you@example.com>
2021-01-16 14:25:07 +0100

    video page better

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,75 @@
 <template lang="pug">
 v-app(dark)
-  v-navigation-drawer(v-model='drawer' :mini-variant='miniVariant' :clipped='clipped' fixed app temporary width=300)
+  v-navigation-drawer(
+    v-model='drawer',
+    :mini-variant='miniVariant',
+    :clipped='clipped',
+    fixed,
+    app,
+    temporary,
+    width=350
+  )
     v-list
-      v-list-item(v-for='(item, i) in items' :key='i' :to='item.to' router exact nuxt)
+      v-list-item(
+        v-for='(item, i) in items',
+        :key='i',
+        :to='item.to',
+        router,
+        exact,
+        nuxt
+      )
         v-list-item-action
           v-icon {{ item.icon }}
         v-list-item-content
           v-list-item-title(v-text='item.title')
-      v-list-group(:value="true" prepend-icon="mdi-account-circle")
-        template(v-slot:activator="")
+      v-list-group(:value='true', prepend-icon='mdi-account-circle')
+        template(v-slot:activator='')
           v-list-item-title Categories
-        v-list-item(v-for="category in this.categories" :key="category.id" :to="`/category/${category.slug}`" router exact dense nuxt)
-            v-list-item-title(v-text="category.name")
+        v-list-item(
+          v-for='category in this.categories',
+          :key='category.id',
+          :to='`/category/${category.slug}`',
+          router,
+          exact,
+          dense,
+          nuxt,
+          v-text='category.name'
+        )
+      
 
-
-        
-
-  v-app-bar(:clipped-left='clipped' fixed app)
+  v-app-bar(:clipped-left='clipped', fixed, app)
     v-app-bar-nav-icon(@click.stop='drawer = !drawer')
-    v-btn(icon @click.stop='miniVariant = !miniVariant')
-      v-icon mdi-{{ &grave;chevron-${miniVariant ? &apos;right&apos; : &apos;left&apos;}&grave; }}
-    v-btn(icon @click.stop='clipped = !clipped')
+    v-btn(icon, @click.stop='miniVariant = !miniVariant')
+      v-icon mdi-{{  &grave;chevron-${miniVariant ? &apos;right&apos; : &apos;left&apos;}&grave;  }}
+    v-btn(icon, @click.stop='clipped = !clipped')
       v-icon mdi-application
-    v-btn(icon @click.stop='fixed = !fixed')
+    v-btn(icon, @click.stop='fixed = !fixed')
       v-icon mdi-minus
     v-toolbar-title(v-text='title')
     v-spacer
-    v-text-field(hide-details prepend-icon="mdi-magnify" single-line)
-      
-      
-    v-btn(icon @click.stop='rightDrawer = !rightDrawer')
+    v-text-field(hide-details, prepend-icon='mdi-magnify', single-line)
+
+    v-btn(icon, @click.stop='rightDrawer = !rightDrawer')
       v-icon mdi-menu
   v-main
     v-container
       nuxt
-  v-navigation-drawer(v-model='rightDrawer' :right='right' temporary fixed)
+  v-navigation-drawer(v-model='rightDrawer', :right='right', temporary, fixed)
     v-list
       v-list-item(@click.native='right = !right')
         v-list-item-action
           v-icon(light)
             | mdi-repeat
         v-list-item-title Switch drawer (click me)
-  v-footer(:absolute='!fixed' app)
+  v-footer(:absolute='!fixed', app)
     span &copy; {{ new Date().getFullYear() }}
-
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  data() {
+  data () {
     return {
       clipped: false,
       drawer: false,
@@ -65,7 +84,7 @@ export default {
           icon: 'mdi-apps',
           title: 'Search videos',
           to: '/search-videos',
-        },        
+        },
         {
           icon: 'mdi-apps',
           title: 'Random video',
@@ -90,7 +109,7 @@ export default {
           icon: 'mdi-apps',
           title: 'Most commented videos',
           to: '/most-commented',
-        },        
+        },
       ],
       categories: [],
       miniVariant: false,
@@ -99,7 +118,7 @@ export default {
       title: 'Sadhguru wisdom',
     }
   },
-  async fetch() {
+  async fetch () {
     this.categories = await this.$axios.$get('/api/categories/')
   },
 }
