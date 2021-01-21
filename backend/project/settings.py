03f0b2b2269f4d4834f92c885c9dba90e579d369
Your Name <you@example.com>
2021-01-21 23:35:44 +0100

    changes

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -94,16 +94,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            "NAME": "dbtest.sqlite3",
-        },
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
