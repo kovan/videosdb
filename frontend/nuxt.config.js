4d435fe7d6a2c851bd4f00f5bde7718d5e1225bd
Your Name <you@example.com>
2021-01-28 16:51:23 +0100

    try to minimize bootstrap css

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@/assets/scss/custom.scss'],
+  // css: ['@/assets/scss/custom.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -39,10 +39,10 @@ export default {
     'bootstrap-vue/nuxt',
   ],
 
-  bootstrapVue: {
-    bootstrapCSS: false,
-    bootstrapVueCSS: false,
-  },
+  // bootstrapVue: {
+  //   bootstrapCSS: false,
+  //   bootstrapVueCSS: false,
+  // },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
