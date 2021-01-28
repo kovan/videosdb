14179ad22e3e00a7e8f2b702a047fcdb85a07ed0
Your Name <you@example.com>
2021-01-28 15:53:32 +0100

    remove vue-youtube

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -21,12 +21,7 @@ export default {
   css: ['@/assets/scss/custom.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    {
-      src: '~/plugins/vue-youtube.js',
-      mode: 'client',
-    },
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
