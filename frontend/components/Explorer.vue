a0d0361075f29a119677a64b2038be28ea1e07f4
Your Name <you@example.com>
2021-01-31 04:26:07 +0100

    css

--- frontend/components/Explorer.vue
+++ frontend/components/Explorer.vue
@@ -43,7 +43,7 @@
     //-     span.carousel-control-next-icon(aria-hidden="true")
     //-     span.sr-only Next
 
-    .album.py-5.bg-light
+    .album.py-1.bg-light
       div
         .row
           .col-md-4(v-for="video in this.videos" :key="video.youtube_id")
@@ -186,71 +186,5 @@ export default {
 
 <style scoped>
 
-
-
-/* CUSTOMIZE THE CAROUSEL
--------------------------------------------------- */
-
-/* Carousel base class */
-/* .carousel {
-  margin-bottom: 4rem;
-} */
-/* Since positioning the image, we need to help out the caption */
-
-
-
-/* MARKETING CONTENT
--------------------------------------------------- */
-
-/* Center align the text within the three columns below the carousel */
-.marketing .col-lg-4 {
-  margin-bottom: 1.5rem;
-  text-align: center;
-}
-.marketing h2 {
-  font-weight: 400;
-}
-.marketing .col-lg-4 p {
-  margin-right: .75rem;
-  margin-left: .75rem;
-}
-
-
-/* Featurettes
-------------------------- */
-
-.featurette-divider {
-  margin: 5rem 0; /* Space out the Bootstrap <hr> more */
-}
-
-/* Thin out the marketing headings */
-.featurette-heading {
-  font-weight: 300;
-  line-height: 1;
-  letter-spacing: -.05rem;
-}
-
-
-/* RESPONSIVE CSS
--------------------------------------------------- */
-
-@media (min-width: 40em) {
-  /* Bump up size of carousel content */
-  .carousel-caption p {
-    margin-bottom: 1.25rem;
-    font-size: 1.25rem;
-    line-height: 1.4;
-  }
-
-  .featurette-heading {
-    font-size: 50px;
-  }
-}
-
-@media (min-width: 62em) {
-  .featurette-heading {
-    margin-top: 7rem;
-  }
-}
 
 </style>
