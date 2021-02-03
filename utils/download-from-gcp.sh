b7a24e453577e07be82de410ca21d502f72bda54
Your Name <you@example.com>
2021-02-03 05:33:28 +0100

    fix

--- utils/download-from-gcp.sh
+++ utils/download-from-gcp.sh
@@ -3,3 +3,15 @@ git checkout prod
 docker-compose pull
 docker-compose up -d
 docker system prune -f
+
+root_url=gcr.io/worpdress-279321/videosdb
+
+for i in backend frontend postgres
+do
+    for sha in $(gcloud container images list-tags $root_url-$i --filter='-tags:*'  --format="get(digest)" --limit=9999999)
+    do
+        gcloud container images delete $root_url-$i@$sha --quiet
+    done
+done
+
+# to delete all images remove the --filterº
