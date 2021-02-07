69d3be573bbdf1798ac77ce3fd44a60df0612cab
Ubuntu <ubuntu@ip-172-31-5-185.ap-south-1.compute.internal>
2021-02-07 15:43:16 +0000

    asdf

--- utils/load_db.sh
+++ utils/load_db.sh
@@ -1,6 +1,8 @@
+export REPO=570555265801.dkr.ecr.ap-south-1.amazonaws.com/videosdb:
 backends="backend-sadhguru backend-nithyananda"
 for backend in $backends
 do
 	gunzip backend/data.$backend.json.gz
+
 	docker-compose run --rm $backend poetry run python manage.py loaddata --format json - < backend/data.$backend.json
 done
