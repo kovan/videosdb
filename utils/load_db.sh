751b8bdc3a89ebf01d7761c8b4e51c0a0774f630
Ubuntu <ubuntu@ip-172-31-46-185.ap-south-1.compute.internal>
2021-02-07 01:17:59 +0000

    changes

--- utils/load_db.sh
+++ utils/load_db.sh
@@ -1,6 +1,6 @@
 backends="backend-sadhguru backend-nithyananda"
 for backend in $backends
 do
-	gunzip $backend.json.gz
-	docker-compose run --rm $backend poetry run python manage.py loaddata --format json -- < $backend.json
+	gunzip backend/data.$backend.json.gz
+	docker-compose run --rm $backend poetry run python manage.py loaddata --format json - < backend/data.$backend.json
 done
