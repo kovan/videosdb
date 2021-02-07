f1e16d0b91ea756e83d8fc7a333c9fb6bff84a59
Your Name <you@example.com>
2021-02-07 02:17:18 +0000

    dumpdb

--- utils/dump_db.sh
+++ utils/dump_db.sh
@@ -1,6 +1,6 @@
 backends="backend-nithyananda"
 for backend in $backends
 do
-    docker-compose run --rm $backend poetry run python manage.py dumpdata --natural-foreign --natuaral-primary > data.$backend.json
+    docker-compose run --rm $backend poetry run python manage.py dumpdata --natural-foreign --natural-primary > data.$backend.json
     gzip data.$backend.json
 done
