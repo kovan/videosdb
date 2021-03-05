a037d1782ed5eef8d0c17d8185216a2dbd43478b
Your Name <you@example.com>
2021-03-05 15:45:30 +0100

    asdf

--- backend/scripts/download_all.py
+++ backend/scripts/download_all.py
@@ -1,15 +1,18 @@
 import logging
 from videosdb.models import Video
 from videosdb.backend.youtube_api import YoutubeDL
+import os
 
+from django.conf import settings
 
 def run():
+    os.chdir(settings.VIDEO_FILES_DIR)
     yt_dl = YoutubeDL()
     for video in Video.objects.filter(excluded=False):
         logging.debug("Downloading " + video.youtube_id)
         try:
             yt_dl.download_video(
-                video.youtube_id, asynchronous=True)
+                video.youtube_id, asynchronous=False)
         except YoutubeDL.UnavailableError as e:
             logging.error(repr(e))
             continue
