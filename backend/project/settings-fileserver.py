230224e546bc0672790dc478170dd8e05f3bd475
Your Name <you@example.com>
2021-03-05 14:38:03 +0100

    asdf

---
+++ backend/project/settings-fileserver.py
@@ -0,0 +1,34 @@
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '',
+        'TEST': {
+            "NAME": "test_videosdb",
+        },
+    },
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sadhguru.sqlite3'),
+        'TEST': {
+            "NAME": "dbtest.sqlite3",
+        },
+    }
+}
+
+
+VIDEOSDB_DOMAIN = "sadhguru.digital"
+VIDEOSDB_DNSZONE = "sadhguru"
+YOUTUBE_CHANNEL = {
+    "id": "UCcYzLCs3zrQIBVHYA1sK2sw",
+    "name": "Sadhguru"
+}
+TRUNCATE_DESCRIPTION_AFTER = "#Sadhguru"
+YOUTUBE_KEY = "AIzaSyAL2IqFU-cDpNa7grJDxpVUSowonlWQFmU"
+VIDEO_FILES_DIR = "/mnt/1tb/Videos"
