89b1d50db6ee1e0f23c64977f4034a8616ecdb58
Your Name <you@example.com>
2021-08-25 00:32:29 +0200

    fix cron

--- utils/cron-task.sh
+++ utils/cron-task.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 POETRY=/home/pi/.local/bin/poetry
-
+DOCKER_COMPOSE=/home/pi/.local/bin/docker-compose
 cd /home/pi/prj/videosdb
-nice -n 11 docker-compose run --rm backend-sadhguru nice -n 10 poetry run python manage.py videosdb --check-for-new-videos
+nice -n 20 $DOCKER_COMPOSE run --rm backend-sadhguru nice -n 20 poetry run python manage.py videosdb --check-for-new-videos
 cd /home/pi/prj/videosdb/backend
-nice -n 11 $POETRY run python -O manage.py videosdb --download-and-register-in-ipfs --settings=project.settings-fileserver
+nice -n 20 $POETRY run python -O manage.py videosdb --download-and-register-in-ipfs --settings=project.settings-fileserver
+
