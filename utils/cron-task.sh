02ce328b53c5048b3a349096f5ff70591c1c1763
Your Name <you@example.com>
2021-08-26 05:51:32 +0200

    sdfa

--- utils/cron-task.sh
+++ utils/cron-task.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 POETRY=/home/pi/.local/bin/poetry
-DOCKER_COMPOSE=/home/pi/.local/bin/docker-compose
+
 cd /home/pi/prj/videosdb
-nice -n 20 $DOCKER_COMPOSE run --rm backend-sadhguru nice -n 20 poetry run python manage.py videosdb --check-for-new-videos
+docker exec videosdb_backend-sadhguru_1 nice -n 20 poetry run python manage.py videosdb --check-for-new-videos
+
 cd /home/pi/prj/videosdb/backend
 nice -n 20 $POETRY run python -O manage.py videosdb --download-and-register-in-ipfs --settings=project.settings-fileserver
 
