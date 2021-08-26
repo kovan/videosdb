f0b34a834bee325c82e53eb44be0512caec2bcc1
Your Name <k@DESKTOP-FBLTVLU.localdomain>
2021-08-27 01:41:04 +0200

    add build commit info to images

--- utils/build.sh
+++ utils/build.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-export REPO="localhost:5000/"
-export TAG="$(git branch --show-current)"
+export REPO="pi:5000/"
+export GIT_COMMIT="$(git rev-parse --verify HEAD)"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 #docker buildx create --name mybuilder 
 #docker buildx use mybuilder
 #docker buildx inspect --bootstrap
-docker buildx bake --set '*.platform=linux/arm/v7' --push
+docker buildx bake --set '*.platform=linux/arm/v7' --push 
