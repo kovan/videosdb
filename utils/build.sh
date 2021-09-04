94328391b3e841f8c293cc08b2fa34be153580fa
Your Name <you@example.com>
2021-09-04 13:44:12 +0200

    tag commit

--- utils/build.sh
+++ utils/build.sh
@@ -2,6 +2,6 @@
 
 sudo service docker start
 export REPO="pi:5000/"
-export GIT_COMMIT="$(git rev-parse --verify HEAD)"
+export TAG="$(git rev-parse --short HEAD)"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx bake --set '*.platform=linux/arm/v7' --push 
