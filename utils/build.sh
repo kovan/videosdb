819770fca39037eb83b7e9bd87ef450b32555ba8
Your Name <you@example.com>
2021-09-13 17:55:15 +0200

    fix repo

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 sudo service docker start
-export REPO=${REPO:-pi:5000/}
+export REPO=${REPO:-desktop-k:5000/}
 export TAG=${TAG:-$(git rev-parse --short HEAD)}
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx bake --push --set '*.platform=linux/arm/v7' 
