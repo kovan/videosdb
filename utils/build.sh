a158b6a817a2b84f855b3435bbdffd48d331ffcd
Your Name <you@example.com>
2021-09-14 23:19:34 +0200

    asdf

--- utils/build.sh
+++ utils/build.sh
@@ -4,4 +4,4 @@ sudo service docker start
 export REPO=${REPO:-desktop-k:5000/}
 export TAG=${TAG:-$(git rev-parse --short HEAD)}
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx bake --push --set '*.platform=linux/arm/v7'  $@
+docker buildx bake --push --set '*.platform=linux/arm/v7' -f docker-compose.yml -f docker-compose.fileserver.yml $@
