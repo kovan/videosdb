03b11b52dd7ecababae837a4dd3823d99771e047
Your Name <you@example.com>
2021-09-14 23:33:34 +0200

    run-fileserver.sh

---
+++ run-fileserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git pull 
+export TAG=$(git rev-parse --short HEAD) 
+export REPO="desktop-k:5000/"
+docker-compose -f docker-compose.fileserver.yml pull --no-parallel
+docker-compose -f docker-compose.fileserver.yml up  --no-build -d
