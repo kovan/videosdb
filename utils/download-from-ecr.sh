7056bb6d32b39a639a420ff437d0a212cbdf8ad6
Your Name <you@example.com>
2021-09-17 01:01:38 +0000

    run-in-aws

--- utils/download-from-ecr.sh
+++
@@ -1,7 +0,0 @@
-git checkout prod
-git pull
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 570555265801.dkr.ecr.ap-south-1.amazonaws.com
-export REPO=570555265801.dkr.ecr.ap-south-1.amazonaws.com/videosdb:
-docker-compose pull
-docker-compose up --no-build -d
-docker system prune -f
