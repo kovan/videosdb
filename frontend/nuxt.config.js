af1b210f4387201b2fa793c9a80410e5d880946a
Your Name <you@example.com>
2021-09-19 01:26:29 +0200

    smaller chunks

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -135,6 +135,10 @@ export default {
     extend(config, ctx) {
       if (ctx.isDev) {
         config.devtool = 'inline-source-map'
+      } else {
+        if (ctx.isClient) {
+           config.optimization.splitChunks.maxSize = 250000
+        }
       }
     },
     loaders:  {
