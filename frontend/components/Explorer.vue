d82195fbcbc912b457a6ad153fd73e888b924dbd
Your Name <you@example.com>
2021-09-22 02:24:46 +0200

    more

--- frontend/components/Explorer.vue
+++ frontend/components/Explorer.vue
@@ -23,30 +23,31 @@
             )
       .row
         .col-md-4(v-for='video in this.videos', :key='video.youtube_id')
-          .card.mb-4.shadow-sm.text-center
-            NuxtLink(:to='"/video/" + video.slug')
-              b-img.bd-placeholder-img.card-img-top(
-                :src='video.thumbnails.medium.url',
-                height='180',
-                width='320',
-                :id='video.youtube_id',
-                :alt='video.title',
-                fluid
-              )
-              b-popover(
-                :target='video.youtube_id',
-                triggers='hover focus',
-                :content='video.description_trimmed'
-              )
-            .card-body
-              p.card-text
-                NuxtLink(:to='"/video/" + video.slug')
-                  | {{ video.title }}
-              .d-flex.justify-content-between.align-items-center
-                small.text-muted Published: {{ new Date(video.yt_published_date).toLocaleDateString() }}
-                small.text-muted Duration: {{ new Date(video.duration_seconds * 1000).toISOString().substr(11, 8) }}
+          LazyHydrate(when-visible)
+            .card.mb-4.shadow-sm.text-center
+              NuxtLink(:to='"/video/" + video.slug')
+                b-img.bd-placeholder-img.card-img-top(
+                  :src='video.thumbnails.medium.url',
+                  height='180',
+                  width='320',
+                  :id='video.youtube_id',
+                  :alt='video.title',
+                  fluid
+                )
+                b-popover(
+                  :target='video.youtube_id',
+                  triggers='hover focus',
+                  :content='video.description_trimmed'
+                )
+              .card-body
+                p.card-text
+                  NuxtLink(:to='"/video/" + video.slug')
+                    | {{ video.title }}
+                .d-flex.justify-content-between.align-items-center
+                  small.text-muted Published: {{ new Date(video.yt_published_date).toLocaleDateString() }}
+                  small.text-muted Duration: {{ new Date(video.duration_seconds * 1000).toISOString().substr(11, 8) }}
       .overflow-auto(v-if='this.videos.length')
-        LazyHidrate(when-visible)
+        LazyHydrate(when-visible)
           b-pagination-nav(
             size='lg',
             align='fill',
