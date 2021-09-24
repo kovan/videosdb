1fcd6bb13d547659f384ee7698e7e8378eebb2a7
Your Name <you@example.com>
2021-09-24 19:49:56 +0200

    asdf

--- utils/build-in-aws.sh
+++ utils/build-in-aws.sh
@@ -3,4 +3,4 @@
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 570555265801.dkr.ecr.ap-south-1.amazonaws.com
 export REPO=570555265801.dkr.ecr.ap-south-1.amazonaws.com/
 
-utils/build.sh
+utils/build.sh $@
