181680d72808e4e3e3acfbdc4ca6d1e0cf4c8798
Your Name <you@example.com>
2021-09-27 07:22:35 +0200

    SSR for youtube embed

--- frontend/pages/Video.vue
+++ frontend/pages/Video.vue
@@ -7,14 +7,13 @@ b-container.m-0.p-0.mx-auto
     .my-4
       h1 {{ this.video.title }}
       p(align='center')
-        client-only
-          youtube(
-            :video-id='this.video.youtube_id',
-            ref='youtube',
-            resize,
-            fitParent,
-            resizeDelay=0
-          )
+        youtube(
+          :video-id='this.video.youtube_id',
+          ref='youtube',
+          resize,
+          fitParent,
+          resizeDelay=0
+        )
 
     .my-4(v-if='this.video.description_trimmed')
       h6 Description
