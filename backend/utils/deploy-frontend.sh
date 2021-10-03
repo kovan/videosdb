2ebd86bad453a9a3af7ffb69acfdb46114c54515
Your Name <you@example.com>
2021-10-03 10:46:34 +0200

    asdf

--- backend/utils/deploy-frontend.sh
+++ backend/utils/deploy-frontend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -d {} "https://webhooks.amplify.ap-south-1.amazonaws.com/prod/webhooks?id=b8787d43-54b9-43dc-98a8-5789c404f9d5&token=vCiiy6rVc8REN3fX4iX0q4wXqWJcn9K4fcUlOMy5gPo&operation=startbuild" -H "Content-Type:application/json"
+curl -X POST -d {} "https://webhooks.amplify.ap-south-1.amazonaws.com/prod/webhooks?id=59ca7f54-f1e9-4733-9169-32c4edc7a7b1&token=I2XWhQu4xtUYXu69WjdhxAM424ZlDbRjTMWj0uh4tY&operation=startbuild" -H "Content-Type:application/json"
