46f2c5d85331312ec4f37f056d09ff33b26d4426
kovan <you@example.com>
2021-11-21 18:25:16 +0100

    seems ok

--- frontend/plugins/myplugin.js
+++ frontend/plugins/myplugin.js
@@ -1,26 +1,5 @@
-import firebase from 'firebase/app';
-import firestore from 'firebase/firestore';
+import { createDb } from "~/utils/utils.server"
 
-function createDb(config) {
-    let db = null;
-    try {
-        db = firebase.firestore()
-    } catch (e) {
-        if (e.name == "FirebaseError") {
-            const firebaseApp = firebase.initializeApp({
-                apiKey: config.apiKey,
-                authDomain: config.authDomain,
-                projectId: config.projectId
-            });
-            db = firebase.firestore()
-        } else
-            throw e
-    }
-    if (process.env.DEBUG) {
-        db.useEmulator("127.0.0.1", 6001);
-    }
-    return db;
-}
 
 
 var db = null
--- frontend/utils/utils.server.js
+++ frontend/utils/utils.server.js
@@ -1,11 +1,29 @@
-function createDb(config) {
-    const firebaseApp = firebase.initializeApp({
-        apiKey: config.apiKey,
-        authDomain: authDomain,
-        projectId: projectId
-    });
+import firebase from 'firebase/app';
+import firestore from 'firebase/firestore';
 
-    return firebase.firestore();
+
+function createDb(config) {
+    let db = null
+    let app = null
+    if (firebase.apps.length == 0) {
+        app = firebase.initializeApp({
+            apiKey: config.apiKey,
+            authDomain: config.authDomain,
+            projectId: config.projectId
+        });
+    } else {
+        app = firebase.apps[0]
+
+    }
+    db = app.firestore();
+    try {
+        if (process.env.DEBUG)
+            db.useEmulator("127.0.0.1", 6001);
+    } catch (e) {
+        if (e.name != "FirebaseError")
+            throw e
+    }
+    return db;
 }
 
 export { createDb }
