39016aa4ee04ee1b7959aef259e601393c593f88
kovan <you@example.com>
2022-09-25 14:30:57 +0200

    wait for port in downloader also

--- backend/tests/test_downloader.py
+++ backend/tests/test_downloader.py
@@ -1,5 +1,3 @@
-import time
-import socket
 import asyncio
 import os
 import aiounittest
@@ -17,31 +15,10 @@ from unittest.mock import create_autospec, patch
 import sys
 
 from videosdb.youtube_api import YoutubeAPI
-
+from videosdb.downloader import wait_for_port
 
 BASE_DIR = os.path.dirname(sys.modules[__name__].__file__)
 DATA_DIR = BASE_DIR + "/test_data"
-
-
-def wait_for_port(port: int, host: str = 'localhost', timeout: float = 5.0):
-    """Wait until a port starts accepting TCP connections.
-    Args:
-        port: Port number.
-        host: Host address on which the port should exist.
-        timeout: In seconds. How long to wait before raising errors.
-    Raises:
-        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
-    """
-    start_time = time.perf_counter()
-    while True:
-        try:
-            with socket.create_connection((host, port), timeout=timeout):
-                break
-        except OSError as ex:
-            time.sleep(0.01)
-            if time.perf_counter() - start_time >= timeout:
-                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
-                                   'connections.'.format(port, host)) from ex
 
 
 def create_mock_httpx_response(filename, status_code=200):
--- backend/videosdb/downloader.py
+++ backend/videosdb/downloader.py
@@ -1,4 +1,6 @@
 import logging
+import socket
+import time
 import bleach
 import os
 import pprint
@@ -19,6 +21,27 @@ import youtube_transcript_api
 
 
 logger = logging.getLogger(__name__)
+
+
+def wait_for_port(port: int, host: str = 'localhost', timeout: float = 5.0):
+    """Wait until a port starts accepting TCP connections.
+    Args:
+        port: Port number.
+        host: Host address on which the port should exist.
+        timeout: In seconds. How long to wait before raising errors.
+    Raises:
+        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
+    """
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
+                                   'connections.'.format(port, host)) from ex
 
 
 def _contains_exceptions(exception_types, exception):
@@ -51,6 +74,7 @@ class Downloader:
 
         if "FIRESTORE_EMULATOR_HOST" in os.environ:
             logger.info("USING EMULATOR")
+            wait_for_port(os.environ["FIRESTORE_EMULATOR_HOST"].split(":")[1])
         else:
             logger.info("USING LIVE DATABASE")
         # for k, v in os.environ.items():
