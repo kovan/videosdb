31f26e8b04b5caba76a68696b3b0aaca11d6c923
kovan <you@example.com>
2022-09-25 03:11:59 +0200

    asdf

--- backend/tests/test_downloader.py
+++ backend/tests/test_downloader.py
@@ -1,3 +1,5 @@
+import time
+import socket
 import asyncio
 import os
 import aiounittest
@@ -19,6 +21,27 @@ from videosdb.youtube_api import YoutubeAPI
 
 BASE_DIR = os.path.dirname(sys.modules[__name__].__file__)
 DATA_DIR = BASE_DIR + "/test_data"
+
+
+def wait_for_port(port: int, host: str = 'localhost', timeout: float = 5.0):
+    """Wait until a port starts accepting TCP connections.
+    Args:
+        port: Port number.
+        host: Host address on which the port should exist.
+        timeout: In seconds. How long to wait before raising errors.
+    Raises:
+        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
+    """
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
+                                   'connections.'.format(port, host)) from ex
 
 
 def create_mock_httpx_response(filename, status_code=200):
@@ -75,6 +98,7 @@ class DownloaderTest(aiounittest.AsyncTestCase):
         load_dotenv("common/env/testing.txt")
         project = os.environ["FIREBASE_PROJECT"]
         config = os.environ["VIDEOSDB_CONFIG"]
+        wait_for_port(8080)
         cls.db = DB.setup(project, config)
 
         cls.VIDEO_IDS = ['FBYoZ-FgC84', 'HADeWBBb1so', 'J-1WVf5hFIk', 'QEkHcPt-Vpw',
--- tests/end2end/run.sh
+++ tests/end2end/run.sh
@@ -9,8 +9,6 @@ docker compose build \
 && \
 docker compose up --detach --remove-orphans \
 && \
-until  nc -z localhost 8080; do sleep 1; done \
-&& \
 docker compose run  --rm -e LOGLEVEL=TRACE backend -m unittest \
 && \
 docker compose run  -e LOGLEVEL=DEBUG --rm backend -O -m videosdb -c -e \
