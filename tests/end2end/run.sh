2d1b44d1f9fab0ca5a75d68fd222aa029105baee
kovan <you@example.com>
2022-09-25 02:59:15 +0200

    asdf

--- tests/end2end/run.sh
+++ tests/end2end/run.sh
@@ -1,30 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 pgrep docker || sudo service docker start
-
-if [[ -z "${PROJECT_ID}" ]]; then
-  echo "USING LOCAL docker-compose file"
-  compose_file="docker-compose.yml"
-else # we are in Google CLoud Builder
-  echo "USING GOOGLE CLOUD BUILD docker-compose file"
-  compose_file="docker-compose.cloudbuild.yml"
-fi
 
 
 
 rm -fr ./dist
 
-docker compose -f $compose_file build \
+docker compose build \
 && \
-docker compose -f $compose_file up --detach --remove-orphans \
+docker compose up --detach --remove-orphans \
 && \
 until  nc -z localhost 8080; do sleep 1; done \
 && \
-docker compose -f $compose_file run  --rm -e LOGLEVEL=TRACE backend -m unittest \
+docker compose run  --rm -e LOGLEVEL=TRACE backend -m unittest \
 && \
-docker compose -f $compose_file run  -e LOGLEVEL=DEBUG --rm backend -O -m videosdb -c -e \
+docker compose run  -e LOGLEVEL=DEBUG --rm backend -O -m videosdb -c -e \
 && \
-docker compose -f $compose_file run frontend yarn generate-and-start\
+docker compose run frontend yarn generate-and-start\
 
-if [[ -z "${PROJECT_ID}" ]]; then
-    docker compose -f $compose_file down
-fi
+
+#docker compose down
+
