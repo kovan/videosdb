e595fa5f6f7aa8009bde8c9199ce2c5eadac8443
kovan <you@example.com>
2022-09-27 05:17:19 +0200

    fix

--- backend/videosdb/downloader.py
+++ backend/videosdb/downloader.py
@@ -131,13 +131,6 @@ class Downloader:
                         firestore.ArrayUnion([playlist_id])
                     })
 
-            if processed_video_ids:
-                ids = list(processed_video_ids)
-                ids.sort()
-                await self.db.set("meta/video_ids", {
-                    "videoIds": firestore.ArrayUnion(ids)
-                })
-
             # retrieve pending transcripts
             if self.options and not self.options.exclude_transcripts:
                 logger.info("Retrieving transcripts")
@@ -157,6 +150,13 @@ class Downloader:
                 return new_state
             else:
                 raise e
+
+        if processed_video_ids:
+            ids = list(processed_video_ids)
+            ids.sort()
+            await self.db.noquota_set("meta/video_ids", {
+                "videoIds": firestore.ArrayUnion(ids)
+            })
 
         new_state = {
             "lastPlaylistId": None,
