164634a64d9a8774e35bf259d8f0bb22adff60f0
kovan <you@example.com>
2022-09-27 02:35:40 +0200

    asdf

--- backend/tests/test_downloader.py
+++ backend/tests/test_downloader.py
@@ -42,8 +42,8 @@ class MockedAPIMixin:
     @classmethod
     def setUpClass(cls):
         load_dotenv("common/env/testing.txt")
-        logger.debug("ENVIRONMENT:")
-        logger.debug(pprint.pformat(os.environ))
+        logger.info("ENVIRONMENT:")
+        logger.info(pprint.pformat(os.environ))
 
         DB.wait_for_port()
 
