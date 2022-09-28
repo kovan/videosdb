ba6922dd73c651bfdf04116ac4373ebb475fe422
kovan <you@example.com>
2022-09-28 23:21:00 +0200

    fix EVENT LOOP CLOSED

--- backend/tests/test_downloader.py
+++ backend/tests/test_downloader.py
@@ -1,8 +1,9 @@
+import asyncio
 import pprint
 from httpx import Response
 import respx
 import os
-import asynctest
+import aiounittest
 import isodate
 import json
 
@@ -21,37 +22,28 @@ BASE_DIR = os.path.dirname(sys.modules[__name__].__file__)
 DATA_DIR = BASE_DIR + "/test_data"
 
 
-class TestDownloader(asynctest.TestCase):
-    VIDEO_IDS = ['FBYoZ-FgC84', 'HADeWBBb1so', 'J-1WVf5hFIk', 'QEkHcPt-Vpw',
-                 'ZhI-stDIlCE', 'ed7pFle2yM8', 'gavq4LM8XK0']
-    PLAYLIST_ID = "PL3uDtbb3OvDMz7DAOBE0nT0F9o7SV5glU"
+class MockedAPIMixin:
+    def get_event_loop(self):  # workaround for bug
+        self.my_loop = asyncio.get_event_loop()
+        return self.my_loop
 
-    async def _clear_db(self):
-        async for col in self.db.collections():
-            await self.db.recursive_delete(col)
+    def setUp(self):
 
-    async def setUp(self):
-        await self._clear_db()
-        # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        # clear DB:
-        # requests.delete(
-        #     "http://%s/emulator/v1/projects/%s/databases/(default)/documents" % (
-        #         os.environ["FIRESTORE_EMULATOR_HOST"],
-        #         os.environ["FIREBASE_PROJECT"]))
-        # time.sleep(0.1)
-
+        self.get_event_loop()
+        self.my_loop.run_until_complete(self._clear_db())
         self.mocked_api.start()
         self.addCleanup(self.mocked_api.stop)
 
-    # def get_event_loop(self):  # workaround for bug
-    #     self.my_loop = asyncio.get_event_loop()
-    #     return self.my_loop
+    async def _clear_db(self):
+        logger.debug("clearing db")
+        async for col in self.db.collections():
+            await self.db.recursive_delete(col)
 
     @classmethod
     def setUpClass(cls):
         load_dotenv("common/env/testing.txt")
 
-        print(pprint.pformat(os.environ))
+        logger.debug(pprint.pformat(os.environ))
 
         DB.wait_for_port()
 
@@ -81,6 +73,12 @@ class TestDownloader(asynctest.TestCase):
         videos = cls.mocked_api.get(path="/videos",  name="videos")
         with open(DATA_DIR + "/video-HADeWBBb1so.response.json") as f:
             videos.return_value = Response(200, json=json.load(f))
+
+
+class DownloaderTest(MockedAPIMixin, aiounittest.AsyncTestCase):
+    VIDEO_IDS = ['FBYoZ-FgC84', 'HADeWBBb1so', 'J-1WVf5hFIk', 'QEkHcPt-Vpw',
+                 'ZhI-stDIlCE', 'ed7pFle2yM8', 'gavq4LM8XK0']
+    PLAYLIST_ID = "PL3uDtbb3OvDMz7DAOBE0nT0F9o7SV5glU"
 
     def test_put_item_at_front(self):
         s = [3, 5, 6, 8]
