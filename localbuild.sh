c8719c1d2f1618d60a365833e81ad0eb96ede7a1
kovan <you@example.com>
2022-10-27 17:14:17 +0200

    asdf

--- localbuild.sh
+++ localbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -q -t videosdb-builder utils/builder
+docker build -t videosdb-builder utils/builder
 docker run --rm --workdir /app  -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app videosdb-builder:latest $*
